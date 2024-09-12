6e4c1f436b17e8b5bf7bd8024d626c78ae8dfe4b
Veerendranath <veerendranath0312@gmail.com>
2024-09-11 21:37:10 -0400

    Learn finding unique values and filtering with DISTINCT and WHERE clauses

--- Chapter_03/Chapter_03.sql
+++ Chapter_03/Chapter_03.sql
@@ -28,3 +28,82 @@ ORDER BY 3 DESC;
 SELECT last_name, school, hire_date
 FROM teachers
 ORDER BY school, hire_date DESC;
+
+-- Listing 3-5: Querying distinct values in the school column
+
+SELECT DISTINCT school
+FROM teachers
+ORDER BY school;
+
+-- Listing 3-6: Querying distinct pairs of values in the school and salary columns
+
+SELECT DISTINCT school, salary
+FROM teachers
+ORDER BY school, salary;
+
+-- Listing 3-7: Filtering rows using WHERE
+
+SELECT last_name, school, hire_date
+FROM teachers
+WHERE school = 'Myers Middle School';
+
+-- Examples of WHERE comparison operators
+
+-- Teachers with first name of Janet
+SELECT first_name, last_name, school
+FROM teachers
+WHERE first_name = 'Janet';
+
+-- School namee not equal to F.D. Roosevelt HS
+SELECT school
+FROM teachers
+WHERE school <> 'F.D. Roosevelt HS';
+
+-- Teachers with hire_date less than January 1, 2000
+SELECT first_name, last_name, school
+FROM teachers
+WHERE hire_date < '2000-01-01';
+
+-- Teachers with salary greater than or equal to $43500
+SELECT first_name, last_name, salary
+FROM teachers
+WHERE salary >= 43500;
+
+-- Teachers with salary in range of $45000 and $65000
+SELECT first_name, last_name, salary
+FROM teachers
+WHERE salary BETWEEN 45000 AND 65000;
+
+SELECT first_name, last_name, school, salary
+FROM teachers
+WHERE salary >= 45000 AND salary <= 65000;
+
+-- Listing 3-8: Filtering with LIKE and ILIKE
+
+SELECT first_name
+FROM teachers
+WHERE first_name LIKE 'sam%';
+
+SELECT first_name
+FROM teachers
+WHERE first_name ILIKE 'sam%';
+
+-- Listing 3-9: Combining operators using AND and OR
+SELECT * FROM teachers
+WHERE school = 'Myers Middle School' AND
+      salary < 40000;
+
+SELECT * FROM teachers
+WHERE last_name = 'Cole' OR
+      last_name = 'Bush';
+
+SELECT * FROM teachers
+WHERE school = 'F. D. Roosevelt HS' AND
+      (salary < 38000 OR salary > 40000);
+
+-- Listing 3-10: A SELECT statement including WHERE and ORDER BY
+
+SELECT *
+FROM teachers
+WHERE school LIKE '%Roos%'
+ORDER BY hire_date DESC;
