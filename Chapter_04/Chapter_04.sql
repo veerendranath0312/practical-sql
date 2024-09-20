007c1697fc48033df1f4d02e64b9305ca0424620
Veerendranath <veerendranath0312@gmail.com>
2024-09-20 15:15:39 -0400

    Understand number data types

--- Chapter_04/Chapter_04.sql
+++ Chapter_04/Chapter_04.sql
@@ -13,3 +13,29 @@ VALUES ('abc', 'abc', 'abc'),
 COPY char_data_types TO '/Users/veeru/Developer/veerendranath/typetest.txt'
 WITH (FORMAT CSV, HEADER, DELIMITER '|');
 
+-- Listing 4-2: Number data types in action
+
+CREATE TABLE number_data_types (
+    numeric_column numeric(20, 5),
+    real_column real,
+    double_column double precision
+);
+
+INSERT INTO number_data_types
+VALUES
+    (.7, .7, .7),
+    (2.13579, 2.13579, 2.13579),
+    (2.1357987654, 2.1357987654, 2.1357987654);
+
+SELECT * FROM number_data_types;
+
+-- Listing 4-3: Rounding issues with float columns
+
+SELECT numeric_column * 10000000 AS fixed,
+       real_column * 10000000 AS floating
+FROM number_data_types
+WHERE numeric_column = 0.7;
+
+
+
+
